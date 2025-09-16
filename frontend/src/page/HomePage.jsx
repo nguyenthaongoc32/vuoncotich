c312a9fccb17ce00ef34d35baf89994828fd8b3b
Nguyen Thao Ngoc <ngocnguyenthao140@gmail.com>
2025-09-16 23:49:42 +0700

    fix home

--- frontend/src/page/HomePage.jsx
+++ frontend/src/page/HomePage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Banner from '../component/homepage/Banner.jsx'
-import JewelryList from "../component/homepage/JewelryList.jsx";
+import JewelryList from '../component/homepage/Jewelry.jsx'
+
 const HomePage = () => {
   return (
     <div>
       <Banner/>
-      <JewelryList/>
+<JewelryList/>
     </div>
   )
 }
